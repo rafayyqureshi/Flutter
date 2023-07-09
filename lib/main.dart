1ac3c2c089b73aa41550cf9f7deb8a836c1e9576
Muhammad Rafay Qureshi <mrafayyqureshi@gmail.com>
2023-07-09 21:15:01 +0500

    Day3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:vulms/home_page.dart';
+import 'package:vulms/pages/home_page.dart';
+import 'package:vulms/pages/login_page.dart';
 
 void main() {
   runApp( MyApp());
@@ -9,7 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
